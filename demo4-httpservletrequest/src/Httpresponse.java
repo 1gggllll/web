d3f4837a9f4a43743597c4a99333942daa44ad25
1ggglll <2767882448@qq.com>
2024-04-23 19:37:49 +0800

    firs

--- demo4-httpservletrequest/src/Httpresponse.java
+++ demo4-httpservletrequest/src/Httpresponse.java
@@ -37,5 +37,21 @@ public class Httpresponse extends HttpServlet {
         writer.write(info);
         //获得一个向响应体中输入二进制信息的字节输出流
         //ServletOutputStream outputStream = resp.getOutputStream();
+        //响应重定向
+        /*
+           1.重定向是通过HttpServletResponse对象实现的
+           2.响应重定向是在服务端提示下的客户端行为
+           3.客户端的地址是变化的
+           4.客户端至少发了两次请求
+           5.请求产生了多次，后端就会有多个request对象，此时请求中的参数不能继续传递
+           6.目标资源可以视图资源
+           7.目标资源不可以是WEB-INF下的资源
+           8.目标资源可以是外部资源
+       */
+        resp.sendRedirect("servlet04");//两种方法都可以的情况下用重定向
+        //resp.sendRedirect("aaaa.html")
+        //resp.sendRedirect("WEB-INF/web-inffffff.html") 不能
+        //resp.sendRedirect("http://www.atguigu.com") 可以
+
     }
 }
